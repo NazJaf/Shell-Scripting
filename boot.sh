48b20d4dfaa55eee035fe1e3beefbcac158a5186
Nazrin Jafarova <163190774+NazJaf@users.noreply.github.com>
2024-09-05 16:46:28 -0700

    Add files via upload

---
+++ boot.sh
@@ -0,0 +1,8 @@
+echo "Would you like to play rock paper scissors? (yes/no)"
+read answer
+
+if [ "$answer" = "yes" ]; then
+    python3 rock_paper_scissors.py
+else
+    echo "That's too bad, maybe next time."
+fi
